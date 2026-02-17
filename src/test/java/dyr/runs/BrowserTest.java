f42a0caee283e93332c46204b74b9fc9b1965caa
shivamyvc <86454634+shivamyvc@users.noreply.github.com>
2026-02-17 22:43:25 +0530

    Added select tag test steps

--- src/test/java/dyr/runs/BrowserTest.java
+++ src/test/java/dyr/runs/BrowserTest.java
@@ -1,5 +1,10 @@
 package dyr.runs;
 
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.List;
+
 import org.openqa.selenium.By;
 import org.testng.annotations.Test;
 
@@ -13,8 +18,12 @@ public class BrowserTest {
 		Browser br= new Browser("ChrOme");
 		br.open("https://swift.techwithjatin.com/login");
 		Thread.sleep(2000);
+		br.type(By.id("username"),"shivammavii");
 		
-		System.out.println(br.isVisible(By.xpath("//buttaaaon[text()]")));
+		br.type(By.id("password"),"Learn@2000");
+		
+		br.getWebElement(By.xpath("//button[text()='Sign In']")).click();
+		
 		
 		
 		
@@ -23,6 +32,23 @@ public class BrowserTest {
 		
 		System.out.println(br.click(By.xpath("//button[text()='Sign In']")));
 		System.out.println(br.click(By.xpath("//button[text()='Sign In']"),6));
+		
+		
+		
+		br.getDriver().navigate().to("https://www.testmuai.com/selenium-playground/select-dropdown-demo/");
+		
+		Thread.sleep(5000);
+		
+		br.selectByVisibleText(By.id("select-demo"), "Monday");
+		
+		List<String> values= new ArrayList<String>();
+		values.add("Ohio");
+		values.add("Texas");
+		values.add("Washington");
+
+		
+		
+		br.selectMultiByVisibleText(By.id("multiselect"), values);
 		
 	}
 
